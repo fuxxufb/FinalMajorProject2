d4d9f111e42897ff55c2fb4d11ebbe5e61143156
Ekko Y <ekko_yang@foxmail.com>
2018-07-08 14:33:31 +0800

    Initial Commit

---
+++ FinalMajorProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FinalMajorProject
+//
+//  Created by Xiaohan Yang on 2018/7/8.
+//  Copyright © 2018年 xiaohan yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FinalMajorProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FinalMajorProject
+//
+//  Created by Xiaohan Yang on 2018/7/8.
+//  Copyright © 2018年 xiaohan yang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
